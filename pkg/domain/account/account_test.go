5c67e3e349498a398ee99ad67bf874f4d90bb4af
Fede Barcelona <fede_rico_94@hotmail.com>
2023-03-22 00:00:54 +0100

    refactor(ci): reoder account tests

--- pkg/domain/account/account_test.go
+++ pkg/domain/account/account_test.go
@@ -12,96 +12,109 @@ var _ = Describe("Account", func() {
 		acc *account.Account
 	)
 
-	BeforeEach(func() {
-		acc = account.NewAccount()
-	})
-
-	It("opens the account", func() {
-		err := acc.OpenAccount("some-id")
-
-		Expect(err).ToNot(HaveOccurred())
-		Expect(acc.ID()).To(Equal(account.ID("some-id")))
-		Expect(acc.IsOpen()).To(BeTrue())
-	})
-
-	When("performing any action on the account", func() {
-		It("fails if the account is not open", func() {
-			err := acc.AddMoney(50)
-
-			Expect(err).To(MatchError(account.ErrAccountIsClosed))
+	When("the account is not yet open", func() {
+		BeforeEach(func() {
+			acc = account.NewAccount()
 		})
 
-		It("fails if the account is not open", func() {
-			err := acc.WithdrawalMoney(50)
-
-			Expect(err).To(MatchError(account.ErrAccountIsClosed))
-		})
-	})
-
-	When("adding money to the account", func() {
-		It("should add the money successfully", func() {
-			_ = acc.OpenAccount("some-id")
-			err := acc.AddMoney(50)
+		It("opens the account correctly", func() {
+			acc := account.NewAccount()
+			err := acc.OpenAccount("some-id")
 
 			Expect(err).ToNot(HaveOccurred())
-			Expect(acc.Balance()).To(Equal(50))
+			Expect(acc.ID()).To(Equal(account.ID("some-id")))
+			Expect(acc.IsOpen()).To(BeTrue())
 		})
 
-		When("the account already had money", func() {
-			It("should return the total balance after adding more money", func() {
-				_ = acc.OpenAccount("some-id")
-				_ = acc.AddMoney(50)
+		When("performing any action on the account", func() {
+			It("fails if the account is not open", func() {
+				err := acc.AddMoney(50)
 
+				Expect(err).To(MatchError(account.ErrAccountIsClosed))
+			})
+
+			It("fails if the account is not open", func() {
+				err := acc.WithdrawalMoney(50)
+
+				Expect(err).To(MatchError(account.ErrAccountIsClosed))
+			})
+		})
+	})
+
+	When("the account is already open", func() {
+		BeforeEach(func() {
+			acc = account.NewAccount(&account.AccountOpened{AccountID: "some-id"})
+		})
+
+		When("adding money to the account", func() {
+			It("should add the money successfully", func() {
 				err := acc.AddMoney(50)
 
 				Expect(err).ToNot(HaveOccurred())
-				Expect(acc.Balance()).To(Equal(100))
+				Expect(acc.Balance()).To(Equal(50))
+			})
+
+			When("the account already had money", func() {
+				It("should return the total balance after adding more money", func() {
+					_ = acc.AddMoney(50)
+
+					err := acc.AddMoney(50)
+
+					Expect(err).ToNot(HaveOccurred())
+					Expect(acc.Balance()).To(Equal(100))
+				})
+			})
+
+			When("trying to add a negative amount", func() {
+				It("fails", func() {
+					err := acc.AddMoney(-1)
+
+					Expect(err).To(MatchError(account.ErrAddMoneyQuantityCannotBeNegative))
+				})
 			})
 		})
 
-		When("trying to add a negative amount", func() {
-			It("fails", func() {
-				_ = acc.OpenAccount("some-id")
-				err := acc.AddMoney(-1)
+		When("removing money from an account", func() {
+			When("the account already had money", func() {
+				It("subtracts the money", func() {
+					_ = acc.AddMoney(50)
 
-				Expect(err).To(MatchError(account.ErrAddMoneyQuantityCannotBeNegative))
+					err := acc.WithdrawalMoney(30)
+
+					Expect(err).ToNot(HaveOccurred())
+					Expect(acc.Balance()).To(Equal(20))
+				})
 			})
-		})
-	})
 
-	When("removing money from an account", func() {
-		When("the account already had money", func() {
-			It("subtracts the money", func() {
-				_ = acc.OpenAccount("some-id")
-				_ = acc.AddMoney(50)
+			When("the account has less money than the amount to withdrawn", func() {
+				It("returns an error", func() {
+					_ = acc.AddMoney(50)
 
-				err := acc.WithdrawalMoney(30)
+					err := acc.WithdrawalMoney(51)
 
-				Expect(err).ToNot(HaveOccurred())
-				Expect(acc.Balance()).To(Equal(20))
-			})
-		})
-
-		When("the account has less money than the amount to withdrawn", func() {
-			It("returns an error", func() {
-				_ = acc.OpenAccount("some-id")
-				_ = acc.AddMoney(50)
-
-				err := acc.WithdrawalMoney(51)
-
-				Expect(err).To(MatchError(account.ErrBalanceIsNotEnoughForWithdrawal))
+					Expect(err).To(MatchError(account.ErrBalanceIsNotEnoughForWithdrawal))
+				})
 			})
 		})
 	})
 
 	When("transferring money to another account", func() {
+		var (
+			origin      *account.Account
+			destination *account.Account
+		)
+		BeforeEach(func() {
+			origin = account.NewAccount(
+				&account.AccountOpened{AccountID: "origin"},
+				&account.AmountAdded{Balance: 100, Quantity: 100},
+			)
+			destination = account.NewAccount(
+				&account.AccountOpened{AccountID: "destination"},
+				&account.AmountAdded{Balance: 30, Quantity: 30},
+			)
+		})
+
 		It("transfers the money correctly", func() {
-			origin := account.NewAccount()
-			_ = origin.OpenAccount("origin")
-			_ = origin.AddMoney(100)
-			destination := account.NewAccount()
-			_ = destination.OpenAccount("destination")
-			_ = destination.AddMoney(30)
 			amount := 50
 
 			err := origin.TransferMoney(amount, destination)
@@ -113,12 +126,7 @@ var _ = Describe("Account", func() {
 
 		When("the origin account has less money than the amount to transfer", func() {
 			It("returns an error", func() {
-				origin := account.NewAccount()
-				_ = origin.OpenAccount("origin")
-				_ = origin.AddMoney(30)
-				destination := account.NewAccount()
-				_ = destination.OpenAccount("destination")
-				amount := 50
+				amount := 101
 
 				err := origin.TransferMoney(amount, destination)
 
