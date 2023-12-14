4647d84ecf22a5736dff17d7395d6ffcbfe4a14b
Fede Barcelona <fede_rico_94@hotmail.com>
2023-12-14 09:47:44 +0100

    fix: make account projection concurrent safe

--- pkg/account/account_projection.go
+++ pkg/account/account_projection.go
@@ -4,19 +4,24 @@ import (
 	"context"
 	"fmt"
 	"sort"
+	"sync"
 
 	"github.com/tembleking/myBankSourcing/pkg/domain"
 	"github.com/tembleking/myBankSourcing/pkg/persistence"
 )
 
 type Projection struct {
-	accountEvents map[string]*Account
+	mutex    sync.RWMutex
+	accounts map[string]*Account
 }
 
 func (a *Projection) Accounts() []*Account {
-	result := make([]*Account, 0, len(a.accountEvents))
+	a.mutex.RLock()
+	defer a.mutex.RUnlock()
 
-	for _, account := range a.accountEvents {
+	result := make([]*Account, 0, len(a.accounts))
+
+	for _, account := range a.accounts {
 		result = append(result, account)
 	}
 
@@ -28,27 +33,30 @@ func (a *Projection) Accounts() []*Account {
 }
 
 func (a *Projection) handleEvent(event domain.Event) {
+	a.mutex.Lock()
+	defer a.mutex.Unlock()
+
 	switch e := event.(type) {
 	case *AccountOpened:
-		a.accountEvents[e.AggregateID()] = NewAccount()
-		a.accountEvents[e.AggregateID()].LoadFromHistory(e)
+		a.accounts[e.AggregateID()] = NewAccount()
+		a.accounts[e.AggregateID()].LoadFromHistory(e)
 	case *AccountClosed:
-		delete(a.accountEvents, e.AccountID)
-	case *AmountDeposited, *AmountWithdrawn, *TransferRequested, *TransferReceived:
-		a.accountEvents[e.AggregateID()].LoadFromHistory(e)
+		delete(a.accounts, e.AccountID)
+	default:
+		a.accounts[e.AggregateID()].LoadFromHistory(e)
 	}
 }
 
 func NewAccountProjection(eventStore *persistence.ReadOnlyEventStore) (*Projection, error) {
-	a := &Projection{accountEvents: map[string]*Account{}}
-
 	events, err := eventStore.LoadAllEvents(context.Background())
 	if err != nil {
 		return nil, fmt.Errorf("error loading all events: %w", err)
 	}
+
+	projection := &Projection{accounts: map[string]*Account{}}
 	for _, event := range events {
-		a.handleEvent(event)
+		projection.handleEvent(event)
 	}
 
-	return a, nil
+	return projection, nil
 }
