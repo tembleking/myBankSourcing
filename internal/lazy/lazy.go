1574511cb60c1711de9247b1d5e8fd27549e02ff
Fede Barcelona <fede_rico_94@hotmail.com>
2024-01-04 09:04:37 +0100

    ci: add tests to lazy

--- internal/lazy/lazy.go
+++ internal/lazy/lazy.go
@@ -5,13 +5,54 @@ import (
 )
 
 type Lazy[T any] struct {
-	value T
-	once  sync.Once
+	value *T
+	mutex sync.RWMutex
 }
 
 func (l *Lazy[T]) GetOrInit(init func() T) T {
-	l.once.Do(func() {
-		l.value = init()
-	})
-	return l.value
+	l.mutex.RLock()
+	if l.value != nil {
+		defer l.mutex.RUnlock()
+		return *l.value
+	}
+	l.mutex.RUnlock()
+
+	l.mutex.Lock()
+	defer l.mutex.Unlock()
+	// we duplicate the check in here because only the first mutex will
+	// succeed adding value to l.value, the rest of goroutines will just
+	// return the value assigned
+	if l.value != nil {
+		return *l.value
+	}
+
+	initValue := init()
+	l.value = &initValue
+	return *l.value
+}
+
+func (l *Lazy[T]) GetOrTryInit(init func() (T, error)) (T, error) {
+	l.mutex.RLock()
+	if l.value != nil {
+		defer l.mutex.RUnlock()
+		return *l.value, nil
+	}
+	l.mutex.RUnlock()
+
+	l.mutex.Lock()
+	defer l.mutex.Unlock()
+	// we duplicate the check in here because only the first mutex will
+	// succeed adding value to l.value, the rest of goroutines will just
+	// return the value assigned
+	if l.value != nil {
+		return *l.value, nil
+	}
+
+	initValue, err := init()
+	if err != nil {
+		return initValue, err
+	}
+
+	l.value = &initValue
+	return *l.value, nil
 }
